022cff6404a14e25653f13e5beae438a65cfef04
Aarav Kohli <aarav.kohli@IDENTOS.ca>
2025-11-02 20:04:17 -0500

    Prepare for PyPI release v0.1.0

--- setup.py
+++ setup.py
@@ -2,9 +2,36 @@ from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import torch
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='bitexact',
+    version='0.1.0',
+    author='Aarav Kohli',
+    author_email='aaravkohli2008@gmail.com',
+    description='Deterministic CUDA operations for reproducible deep learning',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/aaravkohli1/BitExact',
     packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'torch>=2.0.0',
+    ],
     ext_modules=[
         CUDAExtension(
             name='bitexact._C',
@@ -24,7 +51,7 @@ setup(
                 'src',
                 torch.utils.cpp_extension.include_paths()[0],
             ],
-             extra_compile_args={
+            extra_compile_args={
                 'cxx': ['-O3', '-std=c++17'],
                 'nvcc': [
                     '-O3',
